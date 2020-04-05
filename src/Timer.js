602609c6f4d1e82b3938d71453e574a772de0681
github username <github email address>
2020-04-04 20:15:44 -0400

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Timer extends Component {
   constructor() {
@@ -6,19 +6,27 @@ class Timer extends Component {
     this.timer = React.createRef();
     this.state = {
       time: 0,
-      color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+      color: '#' + Math.floor(Math.random() * 16777215).toString(16),
     };
   }
 
   //Your code here
-
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false;
+    }
+    return true;
+  }
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
       this.props.updateInterval * 1000
     );
   }
-
+  componentDidUpdate() {
+    this.timer.current.style.color =
+      '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
   componentWillUnmount() {
     clearInterval(this.interval);
   }
@@ -26,24 +34,24 @@ class Timer extends Component {
   render() {
     const { time, color, logText } = this.state;
     return (
-      <section className="Timer" style={{ background: color }} ref={this.timer}>
+      <section className='Timer' style={{ background: color }} ref={this.timer}>
         <h1>{time}</h1>
         <button onClick={this.stopClock}>Stop</button>
-        <aside className="logText">{logText}</aside>
+        <aside className='logText'>{logText}</aside>
         <small onClick={this.handleClose}>X</small>
       </section>
     );
   }
 
   clockTick = () => {
-    this.setState(prevState => ({
-      time: prevState.time + this.props.updateInterval
+    this.setState((prevState) => ({
+      time: prevState.time + this.props.updateInterval,
     }));
   };
 
   stopClock = () => {
     clearInterval(this.interval);
-    this.setState({ className: "hidden" });
+    this.setState({ className: 'hidden' });
   };
 
   // for the 'x' button,
